9a6595e1aefc2b9c1ea0a40871cd370a42511e02
QA Hoang <qahoang317@gmail.com>
2020-08-12 10:58:27 -0700

    Fix cart UI

--- src/components/cart-dropdown/cart-dropdown.styles.js
+++ src/components/cart-dropdown/cart-dropdown.styles.js
@@ -7,7 +7,7 @@ export const CartDropdownContainer = styled.div`
     height: 340px;
     display: flex;
     flex-direction: column;
-    padding: 20px;
+    padding: 10px;
     border: 1px solid black;
     background-color: white;
     top: 90px;
@@ -29,7 +29,7 @@ export const EmptyMessageContainer = styled.span`
 EmptyMessageContainer.displayName = 'EmptyMessageContainer';
 
 export const CartItemsContainer = styled.div`
-    height: 240px;
+    height: 260px;
     display: flex;
     flex-direction: column;
     overflow: scroll;
